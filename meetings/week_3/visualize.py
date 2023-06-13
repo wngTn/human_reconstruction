79615bfea0edb9e345fe9632a26b155d60a749b7
Tony Wang <tony.wang@tum.de>
2023-06-13 13:12:07 +0200

    just some changes that are still wrong

--- meetings/week_3/visualize.py
+++ meetings/week_3/visualize.py
@@ -18,19 +18,34 @@ extri = parameters["extrinsic_mat"].item(0)
 pcd = o3d.geometry.PointCloud()
 
 for i in range(4):
+    # 3x3 intrinsics matrix
     K = intri
-    intrinsics = o3d.camera.PinholeCameraIntrinsic(512, 512, K[0, 0], K[1, 1], K[0, 2], K[1, 2])
-    extrinsics = np.concatenate((extri[f"cam_T_{i}"], [[0, 0, 0, 1]]), axis=0)
-
-    rgb = o3d.io.read_image(f"data/Synthetic/first_trial/r_0_{i}.png")
+    # intrinsics = o3d.camera.PinholeCameraIntrinsic(512, 512, K[0, 0], K[1, 1], K[0, 2], K[1, 2])
+    # 4 x 4 word -> camera matrix
+    world_2_camera = extri[f"cam_T_{i}"]
+    world_2_camera = np.concatenate((world_2_camera, np.array([[0, 0, 0, 1]])), axis=0)
     d_tmp = Image.open(a[i])
-    d = o3d.geometry.Image(np.asarray(d_tmp)[:, :, 0].astype(np.uint8))
+    # d = o3d.geometry.Image(np.asarray(d_tmp)[:, :, 0].astype(np.uint16))
+    # create 3d point cloud
+    depth_data = np.asarray(d_tmp)[:, :, 0]
+    # get camera to world
+    camera_2_world = np.linalg.inv(world_2_camera)
+    R = camera_2_world[:3, :3]
+    T = camera_2_world[:3, 3]
+    pcd_data = []
+    import itertools
+    for x, y in itertools.product(range(512), range(512)):
+        if depth_data[x, y] == 0:
+            continue
+        px_to_depth_cam = np.dot(np.linalg.inv(K), np.array([x, y, 1])) * depth_data[x, y] / 100
+        world_point = (R.T @ px_to_depth_cam,)[:3] + (-R.T @ T).T
+        pcd_data.append(world_point[0])
 
-    rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(rgb, d, convert_rgb_to_intensity=False, depth_scale=1, depth_trunc=10000000)
-
-    _pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd, intrinsics)
-    _pcd.transform(extrinsics)
+    _pcd = o3d.geometry.PointCloud()
+    _pcd.points = o3d.utility.Vector3dVector(np.array(pcd_data))
     pcd += _pcd
+
+        
 
 
 smpl = o3d.io.read_triangle_mesh("meetings/week_3/0.obj")
