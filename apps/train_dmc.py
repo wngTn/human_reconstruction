c2ef457a1bba44bbece55a3a7068e3b0ded33947
Yushan <coral.zys@gmail.com>
2023-06-25 16:32:40 +0200

    Added: validation code

--- apps/train_dmc.py
+++ apps/train_dmc.py
@@ -26,6 +26,10 @@ from lib.train_util import *
 from lib.data import SyntheticDataset
 from lib.model import *
 
+import trimesh
+import trimesh.proximity
+import trimesh.sample
+
 # get options
 opt = parse_config()
 
@@ -53,7 +57,7 @@ def train(opt):
 
     def set_train():
         netG.train()
-
+    
     if opt.load_netG_checkpoint_path is not None:
         print('loading for net G ...', opt.load_netG_checkpoint_path)
         netG.load_state_dict(torch.load(opt.load_netG_checkpoint_path, map_location=device), strict=False)
@@ -65,7 +69,8 @@ def train(opt):
     print("loaded finished!")
     
     train_dataset = SyntheticDataset(opt, phase='train', num_views=4)
-    test_dataset = SyntheticDataset(opt, phase='test', num_views=4)
+    val_dataset = SyntheticDataset(opt, phase='validation', num_views=4)
+    # test_dataset = SyntheticDataset(opt, phase='test', num_views=4)
         
     projection_mode = train_dataset.projection_mode
     print('projection_mode:', projection_mode)
@@ -75,11 +80,16 @@ def train(opt):
                                    num_workers=opt.num_threads, pin_memory=opt.pin_memory)
     print('train data size: ', len(train_data_loader))
 
+    val_data_loader = DataLoader(val_dataset,
+                                 batch_size=1, shuffle=False,
+                                 num_workers=opt.num_threads, pin_memory=opt.pin_memory)
+    print('val data size: ', len(val_data_loader))
+
     # NOTE: batch size should be 1 and use all the points for evaluation
-    test_data_loader = DataLoader(test_dataset,
-                                  batch_size=1, shuffle=False,
-                                  num_workers=opt.num_threads, pin_memory=opt.pin_memory)
-    print('test data size: ', len(test_data_loader))
+    # test_data_loader = DataLoader(test_dataset,
+    #                               batch_size=1, shuffle=False,
+    #                               num_workers=opt.num_threads, pin_memory=opt.pin_memory)
+    # print('test data size: ', len(test_data_loader))
 
     os.makedirs(opt.checkpoints_path, exist_ok=True)
     os.makedirs(opt.results_path, exist_ok=True)
@@ -128,6 +138,13 @@ def train(opt):
                     iter_net_time - epoch_start_time)
 
             log.add_scalar('loss', error.item(), total_iteration)
+
+            if train_idx % opt.val_step == 0:
+                print("Performing Validation Now")
+                val_loss = validate(netG, netN, val_data_loader, train_idx + (epoch * len(train_dataset)))
+                # log.add_scalar('val_loss', val_loss, epoch)
+                print('Current val loss: ', val_loss)
+
             if train_idx % opt.freq_plot == 0:
                 descrip = 'Name: {0} | Epoch: {1} | {2}/{3} | Err: {4:.06f} | LR: {5:.06f} | Sigma: {6:.02f} | dataT: {7:.05f} | netT: {8:.05f} | ETA: {9:02d}:{10:02d}'.format(
                     opt.name, epoch, train_idx, len(train_data_loader), error.item(), lr, opt.sigma,
@@ -142,19 +159,93 @@ def train(opt):
                 torch.save(optimizerG.state_dict(), '%s/%s/optim_latest' % (opt.checkpoints_path, opt.name))
                 torch.save(optimizerG.state_dict(), '%s/%s/optim_epoch_%d' % (opt.checkpoints_path, opt.name, epoch))
 
-            if train_idx % opt.freq_save_ply == 0:
-                ply_save_path = Path(save_path) / f"{train_idx}.ply"
-                r = res[0].cpu()
-                points = train_data['samples'][0].transpose(0, 1).cpu()
-                save_samples_truncted_prob(ply_save_path, points.detach().numpy(), r.detach().numpy())
+            # if train_idx % opt.freq_save_ply == 0:
+            #     ply_save_path = Path(save_path) / f"{train_idx}.ply"
+            #     r = res[0].cpu()
+            #     points = train_data['samples'][0].transpose(0, 1).cpu()
+            #     save_samples_truncted_prob(ply_save_path, points.detach().numpy(), r.detach().numpy())
 
             iter_data_time = time.time()
-
+        
         # update learning rate
         lr = adjust_learning_rate(optimizerG, epoch, lr, [5, 10, 25], 0.1)
-        train_dataset.clear_cache()
+        # train_dataset.clear_cache()
 
     log.close()
+
+def validate(netG, netN, val_data_loader, train_idx):
+        total_loss = 0.0
+        num_samples = 20
+        total_err = 0
+
+        with torch.no_grad():
+            netG.eval()
+            netN.eval()
+            
+            for i, val_data in tqdm(enumerate(val_data_loader), total=20):
+                if i >= 20:
+                    break
+                for key in val_data:
+                    if torch.is_tensor(val_data[key]):
+                        val_data[key] = val_data[key].to(device=device)
+
+                # predict normal
+                net_normal = netN.forward(val_data['image'])
+                net_normal = net_normal * val_data['mask']
+                
+                val_data['normal'] = net_normal.detach()
+                res, error = netG.forward(val_data)
+
+                ply_save_path = Path("val_vis") / f"pred_{train_idx + i}.ply"
+                r = res[0].cpu()
+                points = val_data['samples'][0].transpose(0, 1).cpu()
+                save_samples_truncted_prob(ply_save_path, points.detach().numpy(), r.detach().numpy())
+
+                ply_gt_path = Path("val_vis") / f"gt_{train_idx + i}.ply"
+                points = val_data['samples'][0].transpose(0, 1)
+                save_samples_truncted_prob(ply_gt_path, points.cpu().detach().numpy(), val_data['labels'][0].cpu().detach().numpy())
+
+                # print("Saved thign for,", train_idx + i)    
+
+                total_err += error               
+                    
+                    # resolution = val_data['vox'].shape[-1]
+                    # coords, _ = create_grid(resolution, resolution, resolution, val_data['b_min'], val_data['b_max'])
+                    # def eval_func(points):
+                    #     points = np.expand_dims(points, axis=0)
+                    #     samples = torch.from_numpy(points).to(device).float()
+                    #     netG.query(samples, val_data['calib'], val_data['extrinsic'])
+                    #     pred = netG.get_preds()[0][0]
+                    #     return pred.detach().cpu().numpy()
+                    # if oct.use_octree:
+                    #     sdf = eval_grid_octree(coords, eval_func, num_samples=num_samples)
+                    # else:
+                    #     sdf = eval_grid(coords, eval_func, num_samples=num_samples)
+
+                    # pts, _ = trimesh.sample.sample_surface(, num_samples)
+                    # _, src_tgt_dist, _ = trimesh.proximity.closest_point(val_data['mesh_path'][0], pts)
+                    # src_tgt_dist[np.isnan(src_tgt_dist)] = 0
+                    # src_tgt_dist[~np.isfinite(src_tgt_dist)] = 0        
+                    # p2s_dist = src_tgt_dist.mean()
+
+                    # total_loss += p2s_dist
+
+                # # save_path = '%s/%s/val_inference_%s_%d.obj' % (opt.results_path, opt.name, val_data['name'], i)
+                # save_root = os.path.join(opt.results_path, opt.name)        #, 'epoch_%s' % epoch
+                # save_path = os.path.joint(save_root, 'val_inference_%d.obj' % (train_idx + i))
+                # image_tensor = val_data['image'].to(device).unsqueeze(0)
+                # mask_tensor = val_data['mask'].to(device).unsqueeze(0)
+                # res = netN.forward(image_tensor)
+                # res = res * mask_tensor
+                # val_data['normal'] = res[0]
+                # print('Saving val mesh to ' + save_path)
+                # gen_mesh_dmc(opt, netG, device, val_data, save_path, use_octree=True)
+
+                # target_root = os.path.join(opt.dataroot, 'Obj')
+                # chamfer = evaluation(save_root, target_root)
+                # total_loss += chamfer
+
+        return total_err / 20
 
 
 if __name__ == '__main__':
